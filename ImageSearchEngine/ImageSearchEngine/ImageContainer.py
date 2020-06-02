b31abc26383826dd9bc9b54ab3fcc93d6ec17d9d
Usama <usamasattar.3347@gmail.com>
2020-06-02 20:05:27 +0500

    Completed ImageContainer class

--- ImageSearchEngine/ImageSearchEngine/ImageContainer.py
+++ ImageSearchEngine/ImageSearchEngine/ImageContainer.py
@@ -9,13 +9,25 @@ V_DIVISIONS = 3
 
 class ImageContainer:
     bin_main = [[0 for i in range(H_DIVISIONS * S_DIVISIONS * V_DIVISIONS )]for j in range(5)]
+    height_main = 0
+    width_main = 0
 
     def __init__ (self, path, main = False):
         self.main = main
         image = cv2.imread(path)
         print(path)
-        self.height, self.width, channel = image.shape
+        #self.height, self.width, channel = image.shape
         
+        #Uncomment this portion if image rescaling is required to fit orginal image size
+        if(self.main == True):
+            ImageContainer.height_main, ImageContainer.width_main, _ = image.shape
+            self.width = ImageContainer.width_main 
+            self.height = ImageContainer.height_main
+        else:
+            image = cv2.resize(image, (ImageContainer.width_main, ImageContainer.height_main))
+            self.width = ImageContainer.width_main
+            self.height = ImageContainer.height_main
+
         #Following portion divides the image into five parts
         center = np.zeros((self.height, self.width, 3), np.uint8)
         center = cv2.ellipse(center, (self.width//2,self.height//2),(int(0.375*self.width),int(0.375*self.height)),0.0,0.0,360.0,(255,255,255),-1)
@@ -51,25 +63,54 @@ class ImageContainer:
         self.region = [topleft, topright, center, bottomleft, bottomright]
 
         self.BinSort()
+        self.Comparator()
 
     def BinSort(self):
-        bin = [[0 for i in range(H_DIVISIONS * S_DIVISIONS * V_DIVISIONS)]for j in range(5)]
+        self.bin = [[0 for i in range(H_DIVISIONS * S_DIVISIONS * V_DIVISIONS)]for j in range(5)]
         for i in range(5):
+            totalpixels = 0
             for x in range(self.width):
                 for y in range(self.height):
                     h, s, v = self.region[i][y, x]
                     if (h != 0 and s != 0 and v != 0):
+                        totalpixels += 1
                         v = math.ceil(v/(255/V_DIVISIONS)) -1
                         s = math.ceil(s/(255/S_DIVISIONS)) -1
                         h = math.ceil(h/(179/H_DIVISIONS)) -1
-                        bin[i][S_DIVISIONS*V_DIVISIONS*h + s*V_DIVISIONS + v] += 1  #Places the pixel in a bin according to HSV value
-        if(self.main == True): ImageContainer.bin_main = bin
-        cv2.imshow("green", self.region[1]);cv2.waitKey();cv2.destroyAllWindows()
-
-    #def Comparator(self):
-    #    if(self.main == False):
-    #        for i in range
+                        self.bin[i][S_DIVISIONS*V_DIVISIONS*h + s*V_DIVISIONS + v] += 1  #Places the pixel in a bin according to HSV value
+            #total = 0
+            for n in range(H_DIVISIONS * S_DIVISIONS * V_DIVISIONS):
+                self.bin[i][n] = round((self.bin[i][n]/totalpixels), 5)
+            #    self.bin[i][n] /= totalpixels
+            #    total += self.bin[i][n]
+            #print(total)
+        if(self.main == True): ImageContainer.bin_main = self.bin
+        #cv2.imshow("green", self.region[1]);cv2.waitKey();cv2.destroyAllWindows()
+            
+    def Comparator(self):
+        #MatchFound = True
+        if(self.main == False):
+            average = 0
+            for i in range(5):
+                average += self.chiSquare(i)
+                print(self.chiSquare(i))
+            average /= 5
+            print(average)
+            if(average <= 1): print("Image match found")
+            else: print("Image match not found")
         
+                #for n in range(H_DIVISIONS * S_DIVISIONS * V_DIVISIONS ):
+                #    if(self.bin[i][n] != ImageContainer.bin_main[i][n]):
+                #        MatchFound = False
+                #        break
+                #if(MatchFound == False):    break
+            #if(MatchFound == True):    print("Image match has been found")
+            #else: print("Image match not found")
+        
+    def chiSquare(self,i):
+        d = 0.5* np.sum([(a-b)**2 / (a+b+float(1e-10)) for (a,b) in zip(self.bin[i],ImageContainer.bin_main[i])])
+        return d
+
 #if __name__ == "__main__":
 #    SourceImage = ImageContainer("F:\\Usama\\Extras\\Untitled.png", main = True)
 #    SourceImage.BinSort()
--- ImageSearchEngine/ImageSearchEngine/ImageSearchEngine.py
+++ ImageSearchEngine/ImageSearchEngine/ImageSearchEngine.py
@@ -1,4 +1,14 @@
 from ImageContainer import *
 
-a =ImageContainer("F:\\Usama\\Extras\\Beethoven.jpg", main = True)
-print(ImageContainer.bin_main)
+a =ImageContainer("F:\\Usama\\Projects\\Programming\\ImageSearchEngine\\Picture Set\\2.jpg", main = True)
+b =ImageContainer("F:\\Usama\\Projects\\Programming\\ImageSearchEngine\\Picture Set\\1.jpg")
+b =ImageContainer("F:\\Usama\\Projects\\Programming\\ImageSearchEngine\\Picture Set\\3.jpg")
+b =ImageContainer("F:\\Usama\\Projects\\Programming\\ImageSearchEngine\\Picture Set\\97.jpg")
+b =ImageContainer("F:\\Usama\\Projects\\Programming\\ImageSearchEngine\\Picture Set\\4.jpg")
+b =ImageContainer("F:\\Usama\\Projects\\Programming\\ImageSearchEngine\\Picture Set\\5.jpg")
+b =ImageContainer("F:\\Usama\\Projects\\Programming\\ImageSearchEngine\\Picture Set\\96.jpg")
+b =ImageContainer("F:\\Usama\\Projects\\Programming\\ImageSearchEngine\\Picture Set\\6.jpg")
+b =ImageContainer("F:\\Usama\\Projects\\Programming\\ImageSearchEngine\\Picture Set\\99.jpg")
+b =ImageContainer("F:\\Usama\\Projects\\Programming\\ImageSearchEngine\\Picture Set\\7.jpg")
+b =ImageContainer("F:\\Usama\\Projects\\Programming\\ImageSearchEngine\\Picture Set\\98.jpg")
+b =ImageContainer("F:\\Usama\\Projects\\Programming\\ImageSearchEngine\\Picture Set\\2.jpg")
