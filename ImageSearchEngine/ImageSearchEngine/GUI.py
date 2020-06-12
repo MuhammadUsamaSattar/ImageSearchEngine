4c701bf42726b81dd9d1ffc5706a43fa7de7051b
Usama <usamasattar.3347@gmail.com>
2020-06-12 20:02:58 +0500

    Developed the first fully functional search engine with GUI

--- ImageSearchEngine/ImageSearchEngine/GUI.py
+++ ImageSearchEngine/ImageSearchEngine/GUI.py
@@ -3,33 +3,57 @@ from PySide2 import QtWidgets,QtGui, QtCore
 import sys
 import ImageFinder
 import pathlib
+import ctypes
+
+WINDOW_WIDTH = ctypes.windll.user32.GetSystemMetrics(0)*0.75
+WINDOW_HEIGHT = ctypes.windll.user32.GetSystemMetrics(1)*0.75
+RATIO = WINDOW_WIDTH/WINDOW_HEIGHT
 
 def pathResolver(path):
     newpath = ""
-    characters = ()
+    characters = []
     for i in range(len(path)):
         if (path[i] == "/" and i != 0):
-            characters.append("//")
+            characters.append("\\\\")
         else:
             characters.append(path[i])
+    characters = tuple(characters)
     return newpath.join(characters)
+
+
 class GUI(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("Hello")
-        self.setGeometry(300,300,600,700)
+        self.setGeometry(WINDOW_WIDTH/6,WINDOW_HEIGHT/6,WINDOW_WIDTH,WINDOW_HEIGHT)
         
-        self.menu()
+        #self.menu()
         self.searchImagesButton()
         self.numberSelector()
+        self.tagSelector()
+        self.destpath()
+        self.textWriter()
+        self.setMainImage()
         self.show()
         
     def searchImagesButton(self):
         self.btn = QtWidgets.QPushButton("Search Online",self)
         self.btn.resize(self.btn.minimumSizeHint())
-        self.btn.move(100,150)
+        self.btn.move(14*WINDOW_WIDTH/16,5*WINDOW_HEIGHT/8)
         self.btn.clicked.connect(self.searchOnline)
+
+    def destpath(self):
+        self.destpathButton = QtWidgets.QPushButton("Select Destination",self)
+        self.destpathButton.resize(self.destpathButton.minimumSizeHint())
+        self.destpathButton.move(13*WINDOW_WIDTH/16,6*WINDOW_HEIGHT/8)
+        self.destpathButton.clicked.connect(self.dest)
+
+    def setMainImage(self):
+        self.mainImageButton = QtWidgets.QPushButton("Set Man File",self)
+        self.mainImageButton.resize(self.mainImageButton.minimumSizeHint())
+        self.mainImageButton.move(13*WINDOW_WIDTH/16,5*WINDOW_HEIGHT/8)
+        self.mainImageButton.clicked.connect(self.browsewindow)
 
     def menu(self):
         browse_option = QtWidgets.QAction("&Browse...", self)
@@ -54,31 +78,75 @@ class GUI(QtWidgets.QMainWindow):
         print(self.filepath)
         self.filepath = pathResolver(str(self.filepath))
         print(self.filepath)
-        self.imageDisplay()
+        #self.imageDisplay()
 
     def numberSelector(self):
         self.numberSelectorBox = QtWidgets.QLineEdit(self)
         self.numberSelectorBox.setMaxLength(3)
+        self.numberSelectorBox.resize(self.numberSelectorBox.minimumSizeHint())
         self.numberSelectorBox.setText("5")
-        self.numberSelectorBox.move(400,600)
+        self.numberSelectorBox.move(13*WINDOW_WIDTH/16,1.5*WINDOW_HEIGHT/16)
         validator = QtGui.QIntValidator()
         self.numberSelectorBox.setValidator(validator)
-        self.numberSelectorBox.textEdited.connect(self.assignnum)
+        self.numberSelectorBox.textEdited.connect(self.assignNum)
+
+    def tagSelector(self):
+        self.tagSelectorBox = QtWidgets.QLineEdit(self)
+        self.tagSelectorBox.setMaxLength(100)
+        self.tagSelectorBox.resize( self.tagSelectorBox.minimumSizeHint())
+        self.tagSelectorBox.setFixedWidth(150)
+        self.tagSelectorBox.setText("")
+        self.tagSelectorBox.move(13*WINDOW_WIDTH/16,2.5*WINDOW_HEIGHT/16)
+        self.tagSelectorBox.textEdited.connect(self.assignTag)
 
     def imageDisplay(self):
-        pixmap = QtGui.QPixmap(fileName = str(pathlib.Path(self.filepath)))
-        pixmap = pixmap.scaled(200,200,QtCore.Qt.KeepAspectRatio)
-        displayimage = QtWidgets.QLabel()
+        pixmap = QtGui.QPixmap(self.filepath)
+        pixmap = pixmap.scaledToWidth(12*WINDOW_WIDTH/16)
+        displayimage = QtWidgets.QLabel(parent = self)
         displayimage.setPixmap(pixmap)
-        displayimage.resize(200,200)
         self.setCentralWidget(displayimage)
 
+    def textWriter(self):
+        textNum = QtWidgets.QLabel("Num",self)
+        textNum.move(13*WINDOW_WIDTH/16,2*WINDOW_HEIGHT/32)
+        textTag = QtWidgets.QLabel("Tag",self)
+        textTag.move(13*WINDOW_WIDTH/16,4*WINDOW_HEIGHT/32)
+
+    def paintEvent(self, e):
+        painter = QtGui.QPainter()
+        painter.begin(self)
+        brush = QtGui.QBrush(QtCore.Qt.SolidPattern)
+
+        brush.setColor(QtGui.QColor(190,190,190))
+        painter.setBrush(brush)
+        painter.drawRect(0, 0, 12*WINDOW_WIDTH/16,WINDOW_HEIGHT)
+        
+        brush.setColor(QtGui.QColor(235,235,235))
+        painter.setBrush(brush)
+        painter.drawRect(12.0625*WINDOW_WIDTH/16,0,3.9375*WINDOW_WIDTH/16,WINDOW_HEIGHT)
+
+        brush.setStyle(QtCore.Qt.Dense5Pattern)
+        brush.setColor(QtGui.QColor(255,255,255))
+        painter.setBrush(brush)
+        painter.drawRect(12*WINDOW_WIDTH/16,0,WINDOW_WIDTH/256,WINDOW_HEIGHT)
+
+        painter.end()
+
     def searchOnline(self):
-        ImageFinder.ImageFinder(self.filepath,"Sea", num = self.num)
+        ImageFinder.ImageFinder(self.filepath, self.tag, self.destpath, self.num)
     
-    def assignnum(self):
+    def dest(self): 
+        self.destpath = QtWidgets.QFileDialog.getExistingDirectory(self, "Select desired destination folder","\\home\\", QtWidgets.QFileDialog.ShowDirsOnly )
+        print(self.destpath)
+        self.destpath = pathResolver(str(self.destpath))
+        print(self.destpath)
+
+    def assignNum(self):
         self.num = int(self.numberSelectorBox.text())
         print(self.num)
+
+    def assignTag(self):
+        self.tag = str(self.tagSelectorBox.text())
 
     def exit(self):
         sys.exit()
--- ImageSearchEngine/ImageSearchEngine/ImageFinder.py
+++ ImageSearchEngine/ImageSearchEngine/ImageFinder.py
@@ -5,10 +5,11 @@ import os
 import sys
 
 class ImageFinder:
-    def __init__(self, sourcePath, keyword, num):
+    def __init__(self, sourcePath, keyword, destpath="Image Database" ,num = 5):
         self.sourcePath = sourcePath
         self.keyword = keyword
-        bingcrawler = BingImageCrawler(storage={'root_dir':'Image Database\\'+self.keyword+'\\'})
+        self.destpath = destpath
+        bingcrawler = BingImageCrawler(storage={'root_dir':self.destpath+"\\"+self.keyword+'\\'})
         bingcrawler.crawl(keyword=self.keyword,max_num = num)
         self.Sorter()
 
@@ -17,7 +18,7 @@ class ImageFinder:
         SimilarityIndex = []
         OrignalFileNumber = []
         count = 0
-        for path in pathlib.Path('Image Database\\'+self.keyword+'\\').iterdir():
+        for path in pathlib.Path(self.destpath+'\\'+self.keyword+'\\').iterdir():
             tempImageContainer = ImageContainer(str(path))
             SimilarityIndex.append(tempImageContainer.Comparator())
             OrignalFileNumber.append(count+1)
@@ -38,16 +39,16 @@ class ImageFinder:
 
         for x in range(k):
             if(OrignalFileNumber[x]/10 < 1):
-                os.rename('Image Database\\'+self.keyword+'\\00000'+str(OrignalFileNumber[x])+'.jpg','Image Database\\'+self.keyword+'\\Sorted_00000'+str(x+1)+'.jpg')
+                os.rename(self.destpath+'\\'+self.keyword+'\\00000'+str(OrignalFileNumber[x])+'.jpg',self.destpath+'\\'+self.keyword+'\\Sorted_00000'+str(x+1)+'.jpg')
             elif(OrignalFileNumber[x]/100 < 1):
-                os.rename('Image Database\\'+self.keyword+'\\0000'+str(OrignalFileNumber[x])+'.jpg','Image Database\\'+self.keyword+'\\Sorted_00000'+str(x+1)+'.jpg')
+                os.rename(self.destpath+'\\'+self.keyword+'\\0000'+str(OrignalFileNumber[x])+'.jpg',self.destpath+'\\'+self.keyword+'\\Sorted_00000'+str(x+1)+'.jpg')
             elif(OrignalFileNumber[x]/1000 < 1):
-                os.rename('Image Database\\'+self.keyword+'\\000'+str(OrignalFileNumber[x])+'.jpg','Image Database\\'+self.keyword+'\\Sorted_00000'+str(x+1)+'.jpg')
+                os.rename(self.destpath+'\\'+self.keyword+'\\000'+str(OrignalFileNumber[x])+'.jpg',self.destpath+'\\'+self.keyword+'\\Sorted_00000'+str(x+1)+'.jpg')
             elif(OrignalFileNumber[x]/10000 < 1):
-                os.rename('Image Database\\'+self.keyword+'\\00'+str(OrignalFileNumber[x])+'.jpg','Image Database\\'+self.keyword+'\\Sorted_00000'+str(x+1)+'.jpg')
+                os.rename(self.destpath+'\\'+self.keyword+'\\00'+str(OrignalFileNumber[x])+'.jpg',self.destpath+'\\'+self.keyword+'\\Sorted_00000'+str(x+1)+'.jpg')
             elif(OrignalFileNumber[x]/100000 < 1):
-                os.rename('Image Database\\'+self.keyword+'\\0'+str(OrignalFileNumber[x])+'.jpg','Image Database\\'+self.keyword+'\\Sorted_00000'+str(x+1)+'.jpg')
+                os.rename(self.destpath+'\\'+self.keyword+'\\0'+str(OrignalFileNumber[x])+'.jpg',self.destpath+'\\'+self.keyword+'\\Sorted_00000'+str(x+1)+'.jpg')
 
-        f = open("Image Database\\"+self.keyword+"\\Similarity Indices.txt", "w")
+        f = open(self.destpath+'\\'+self.keyword+"\\Similarity Indices.txt", "w")
         for i in range(count):
             f.write("File Number: {:>5}    |     Similarity Index: {:<18}     |\n".format(str(i+1),str(SimilarityIndex[i])))
